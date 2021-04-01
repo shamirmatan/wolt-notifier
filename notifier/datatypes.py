c60ee03cdd2c5c9a6276692f12219af853620e7b
Matan Shamir <matans8890@gmail.com>
2021-04-01 19:37:37 +0300

    change get req

--- notifier/datatypes.py
+++ notifier/datatypes.py
@@ -5,15 +5,17 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Dict
 
+
 class Geo(Enum):
     LAT = 0
     LON = 1
+
 
 @dataclass
 class Restaurant:
     name: str
     online: bool = False
-    api = 'https://restaurant-api.wolt.com/v1/'
+    api = "https://restaurant-api.wolt.com/v1/search"
 
     @staticmethod
     def _get_my_location():
@@ -21,12 +23,23 @@ class Restaurant:
         return my_location.latlng
 
     def _search_restaurant(self, restaurant: str, lat: float, lon: float) -> Dict:
-        x = requests.get(f'{self.api}search?sort=relevancy&q={restaurant}&lat={lat}&lon={lon}&limit=20')
-        x_dict = json.loads(x.text)
-        return x_dict["results"]
-    
+        x = requests.get(
+            self.api,
+            params={
+                "sort": "relevancy",
+                "q": restaurant,
+                "lat": lat,
+                "lon": lon,
+                "limit": 20,
+            },
+        )
+        return json.loads(x.text)["results"]
+
     def _is_target_restaurant(self, restaurant: str) -> bool:
-        return restaurant["value"].get("name")[0]["value"] == self.name
+        if self.name in restaurant["value"].get("name")[0]["value"]:
+            return True
+        return False
+        # return restaurant["value"].get("name")[0]["value"] == self.name
 
     def _get_restaurant(self, restaurant: str) -> Dict:
         location = Restaurant._get_my_location()
